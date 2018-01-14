4a3e0902c459403137f9b189ae1efd7ca3f6596b
Saji Ayahito <saji.ayahito@h.mbox.nagoya-u.ac.jp>
2018-01-14 10:17:02 +0900

    認証(authentications)コントローラを作成。

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'rooms#new'
+
+  resources :rooms, only: [:new, :create, :destroy]
+  resources :authentications, only: [:new, :create, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
