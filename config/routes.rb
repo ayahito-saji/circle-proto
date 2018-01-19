0e6ca647c3eb3baf9c610cb835b319ea395554de
Saji Ayahito <saji.ayahito@h.mbox.nagoya-u.ac.jp>
2018-01-19 13:51:56 +0900

    コントローラ消したので、ルート設定も全削除。

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  root 'rooms#new'
-  get 'login', to: 'rooms#new'
-  post 'login', to: 'rooms#create'
-  delete 'logout', to: 'rooms#exit'
-  get 'room', to: 'rooms#view'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
