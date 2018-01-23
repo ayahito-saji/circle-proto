0f1ab6b85274aafcb399965abe75f03fb07fba08
Saji Ayahito <saji.ayahito@h.mbox.nagoya-u.ac.jp>
2018-01-24 00:49:53 +0900

    roomsコントローラを作成。

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,13 @@
+class RoomsController < ApplicationController
+  before_action :require_login
+  def create
+    @room = Room.new
+    @room.maximum = -1 if current_user.premium?
+    if @room.save
+      redirect_to root_path
+    else
+      flash.now[:danger] = "Create room error."
+      redirect_to root_path
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,12 @@ Rails.application.routes.draw do
   post "signup", to: "users#create"
   delete "signout", to: "users#destroy"
 
-
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   get "account", to: "sessions#show"
   delete "logout", to: "sessions#destroy"
+
+  post "create_room", to: "rooms#create"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
