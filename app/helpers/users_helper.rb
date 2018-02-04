f02f6277fffb0108b7a9e368634df87bc4d5bf48
Saji Ayahito <saji.ayahito@h.mbox.nagoya-u.ac.jp>
2018-02-04 14:39:33 +0900

    ユーザーアイコンを削除(外部サービスであるgravatarを利用すると、 編集のためにgravatarのサイトへいかなければならないため)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,9 +6,4 @@ module UsersHelper
       "Sign up"
     end
   end
-  def icon_for(user, size: 80)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name)
-  end
 end
